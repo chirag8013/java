1aa8ccaa56e8729e13b4c0a52905421bbdea5121
Girma <GirmaS@moodys.com>
2020-10-06 20:09:50 -0400

    remove eclipse files

---
+++ src/test/java/com/moodys/qats/utilities/TestBase.java
@@ -0,0 +1,47 @@
+package com.moodys.qats.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestBase {
+
+	public Properties prop;
+
+	public WebDriver driver;
+
+	
+
+	public  Properties initialization() throws IOException{
+		FileInputStream ip = new FileInputStream(
+				"src/test/resources/Config.Properties");
+		prop = new Properties();
+
+		prop.load(ip);
+		return prop;
+
+		
+	}
+	
+	public WebDriver launchthebrowser(){
+		
+		System.setProperty("webdriver.chrome.driver",
+				"src/test/resources/Drivers/chromedriver.exe");
+		/* Launching the browser? */
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		options.addArguments("window-size=1920,1080");
+		driver = new ChromeDriver(options);
+
+//		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+		return driver;
+	}
+}
