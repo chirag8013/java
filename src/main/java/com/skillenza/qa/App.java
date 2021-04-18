e5a59d5f12e05558e1e717da16643130113a52a6
Verma <VermaC@moodys.com>
2021-04-18 14:25:03 +0530

    raw

---
+++ src/main/java/com/skillenza/qa/App.java
@@ -0,0 +1,56 @@
+package com.skillenza.qa;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver","chrome_driver/chromedriver2.exe");
+		
+		//WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://return-order-app.herokuapp.com/";
+		driver.get(baseUrl);
+
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("1257945872");
+
+		driver.findElement(By.xpath("//button[text()]")).click();
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scroll(0,4000)");
+		
+		Thread.sleep(3500);
+
+		driver.findElement(By.xpath("//button[text()='Return This Product']")).click();
+		Thread.sleep(3500);
+
+		WebElement result_div = driver.findElement(By.xpath("//div[@class='result']"));
+
+		String result_str = result_div.getText();
+
+		try {
+			FileWriter myWriter = new FileWriter("result.txt");
+			myWriter.write(result_str);
+			myWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		driver.close();
+
+	}
+}
